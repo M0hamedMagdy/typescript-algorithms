5112598afe989491fb0c808465ae0fb812204671
mohamed magdy <mohamedmagdy12@outlook.com>
2023-08-29 22:03:25 +0300

    Adding Bubble Sort

---
+++ sorting/bubbleSort/index.tsx
@@ -0,0 +1,14 @@
+// Bubble Sort
+// You have an array from 0 to N but exclude N or N+1, don't go off the array.
+
+export default function bubbleSort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+}
+
+// O(n2)
