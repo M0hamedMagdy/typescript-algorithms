1e8fdb63d95436a0931e487d070861b4944e1ed0
M0hamedMagdy <mohamedmagdy12@outlook.com>
2023-09-11 19:01:58 +0300

    Fixing bubbleSort

--- sorting/bubbleSort/index.tsx
+++ sorting/bubbleSort/index.tsx
@@ -1,7 +1,7 @@
 // Bubble Sort
 // You have an array from 0 to N but exclude N or N+1, don't go off the array.
 
-export default function bubbleSort(arr: number[]): void {
+export default function bubbleSort(arr: number[]): number[] {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -9,6 +9,7 @@ export default function bubbleSort(arr: number[]): void {
       }
     }
   }
+  return arr
 }
 
 // O(n2)
